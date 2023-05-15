165c033feb8d3502ae403aa8c3eb18fe1e79945c
ikhdaamel <ikhda.new45@gmail.com>
2023-05-15 11:26:55 +0700

    membuat member

--- staticmember_131/staticmember_131.cpp
+++ staticmember_131/staticmember_131.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 class mahasiswa {
 public:
-	 
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pNama) : nama(pNama) { setID(); }
 };
