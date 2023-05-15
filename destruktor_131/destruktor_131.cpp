1f08bb4e4d4930f61213d47cf78533dc2f4d318e
ikhdaamel <ikhda.new45@gmail.com>
2023-05-15 16:39:58 +0700

    menambahkan void

--- destruktor_131/destruktor_131.cpp
+++ destruktor_131/destruktor_131.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 using namespace std;
 
-class angka {
+class angka{
 private:
-	int* arr;
+	int *arr;
 	int panjang;
 public:
 	angka(int);				//constructor
 	~angka();				//destructor
 	void cetakData();
-	void isidata();
+	void isiData();
 };
+
+//definisi member functon
+angka::angka(int i) {			//constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() {				//destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "alamat array sudah dilepaskan" <<endl;
+}
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = "; cin >> arr[i];
+	}
+	cout << endl;
+}
